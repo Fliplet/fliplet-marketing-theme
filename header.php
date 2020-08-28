da33cee2037680c04c0d26aa5d8facafe9c9a1ac
Hugo Carneiro <hello@thegeekdeveloper.com>
2020-08-28 13:27:50 +0100

    Added WordPress menus

--- header.php
+++ header.php
@@ -39,57 +39,36 @@
 
 <body <?php global $blog_id; body_class('site-id-' . $blog_id);?>>
 
-	<header id="header">
-		<div class="menu-bar">
-			<a class="menu-bar-fliplet-wrapper" aria-label="Home" href="/">
-        <img class="menu-bar-fliplet-icon" src="/wp-content/uploads/2018/03/Ribbon_Colour.png" alt="Fliplet Icon">
-        <img class="menu-bar-fliplet-text" src="/wp-content/uploads/2018/11/fliplet-menu-text.png" alt="Fliplet">
-      </a>
-			<a href="https://studio.fliplet.com/signin" aria-label="Pricing"><div class="menu-link"><p>LOG IN</p></div></a>
-			<a href="/pricing" aria-label="Pricing"><div class="menu-link"><p>PRICING</p></div></a>
-			<div class="menu-link" id="resources-link"><p>RESOURCES</p>
-				<div class="sub-menu" id="resources-link-sub-menu">
-					<div class="sub-menu-column">
-						<a href="https://help.fliplet.com/" aria-label="Fliplet support site" target="_blank"><div class="sub-menu-link">Support site</div></a>
-						<a href="https://code.fliplet.com/code-library/" aria-label="Fliplet code library" target="_blank"><div class="sub-menu-link">Code library</div></a>
-						<a href="http://developers.fliplet.com" aria-label="Fliplet developer site" target="_blank"><div class="sub-menu-link">Developer site</div></a>
-						<a href="/contact" aria-label="Contact us"><div class="sub-menu-link">Contact us</div></a>
-						<a href="/reports" aria-label="Reports"><div class="sub-menu-link">Reports</div></a>
-					</div>
-					<div class="sub-menu-column">
-						<a href="https://studio.fliplet.com" aria-label="Fliplet Studio login" target="_blank"><div class="sub-menu-link">Studio login</div></a>
-						<a href="/fliplet-viewer" aria-label="Fliplet Viewer download"><div class="sub-menu-link">Fliplet viewer</div></a>
-						<a href="/blog" aria-label="Blog"><div class="sub-menu-link">Blog</div></a>
-						<a href="/fliplet-careers" aria-label="Contact us"><div class="sub-menu-link">Careers</div></a>
-						<a href="/webinars" aria-label="Webinars"><div class="sub-menu-link">Webinars</div></a>
-					</div>
-				</div>
-			</div>
-			<a href="/app-gallery" aria-label="App gallery"><div class="menu-link"><p>APP GALLERY</p></div></a>
 
-      <div class="menu-link" id="features-link"><p>FEATURES</p>
-				<div class="sub-menu" id="features-link-sub-menu">
-					<div class="sub-menu-column">
-						<a href="/creating-prefab-apps-fliplet-studio/" aria-label="How it works"><div class="sub-menu-link">App creation features</div></a>
-						<a href="/data-storage-integrations-security/" aria-label="data integrations, storage and security"><div class="sub-menu-link">Data & integrations</div></a>
-            <a href="/process-logic-workflow/" aria-label="workflow and process logic"><div class="sub-menu-link">Workflow and process logic</div></a>
-						<a href="/extend-fliplet-code/" aria-label="Extending Fliplet with code"><div class="sub-menu-link">Extending Fliplet</div></a>
-						<a href="/technical-details/" aria-label="compare Fliplet to alternatives"><div class="sub-menu-link">Info for IT teams</div></a>
-						<a href="/security/" aria-label="Fliplet security"><div class="sub-menu-link">Security</div></a>
-					</div>
-				</div>
-			</div>
-      <div class="menu-link" id="why-fliplet-link"><p>WHY FLIPLET?</p>
-				<div class="sub-menu" id="why-fliplet-link-sub-menu">
-					<div class="sub-menu-column">
-						<a href="/prefab-app-builder/" aria-label="Why use Prefab Apps?"><div class="sub-menu-link">Why use Prefab Apps?</div></a>
-						<a href="/compare-fliplet-alternatives/" aria-label="Compare Fliplet to alternatives"><div class="sub-menu-link">Compare Fliplet to alternatives</div></a>
-						<a href="/case-studies" aria-label="Fliplet case studies"><div class="sub-menu-link">Case studies</div></a>
-					</div>
-				</div>
-			</div>
-			<a href="https://studio.fliplet.com/signup" aria-label="Create a free account"><div class="menu-link"><p>FREE ACCOUNT</p></div></a>
-		</div>
+	<header id="header">
+		<?php
+			$attachmentLogo = wp_get_attachment_by_post_name( 'Ribbon_Colour' );
+			$attachmentText = wp_get_attachment_by_post_name( 'fliplet-menu-text' );
+
+			if ( $attachmentLogo ) {
+				$imageLogo = wp_get_attachment_image_src($attachmentLogo->ID, 'full');
+			}
+
+			if ( $attachmentText ) {
+				$imageText = wp_get_attachment_image_src($attachmentText->ID, 'full');
+			}
+		?>
+		<a class="menu-bar-fliplet-wrapper" aria-label="Home" href="/">
+			<img class="menu-bar-fliplet-icon" src="<?php echo $imageLogo[0]; ?>" alt="Fliplet Icon">
+			<img class="menu-bar-fliplet-text" src="<?php echo $imageText[0]; ?>" alt="Fliplet">
+		</a>
+
+		<?php
+			wp_nav_menu(
+				array(
+					'theme_location' => 'main_menu',
+					'menu_class' => 'navbar-nav',
+					'container' => 'div',
+					'container_class' => 'menu-bar',
+					'walker' => new Nav_Menu_Walker(),
+				)
+			);
+		?>
 
 		<div class="menu-hamburger">
 			<span></span>
@@ -99,58 +78,32 @@
 
 		<div class="menu-overlay">
 			<div class="menu-overlay-main">
-				<div class="menu-overlay-title">MAIN MENU</div>
-				<a href="https://studio.fliplet.com/signup" aria-label="Create a free account">
-					<div class="menu-overlay-link">Create a free account</div>
-				</a>
-				<a href="/prefab-app-builder/" aria-label="Why use prefab apps">
-					<div class="menu-overlay-link">Why use prefab apps?</div>
-				</a>
-				<a href="/creating-prefab-apps-fliplet-studio/" aria-label="How it works">
-					<div class="menu-overlay-link">App creation features</div>
-				</a>
-        <a href="/process-logic-workflow/" aria-label="Workflow and process logic">
-					<div class="menu-overlay-link">Workflow and process logic</div>
-				</a>
-				<a href="/app-gallery" aria-label="App gallery">
-					<div class="menu-overlay-link">App gallery</div>
-				</a>
-				<a href="/compare-fliplet-alternatives" aria-label="Compare Fliplet to alternatives">
-					<div class="menu-overlay-link">Compare Fliplet to alternatives</div>
-				</a>
-				<a href="/case-studies" aria-label="Case studies">
-					<div class="menu-overlay-link">Case studies</div>
-				</a>
-				<a href="/pricing/" aria-label="Pricing">
-					<div class="menu-overlay-link">Pricing</div>
-				</a>
-				<a href="/extend-fliplet-code/" aria-label="Extending Fliplet with code">
-					<div class="menu-overlay-link">Extending Fliplet</div>
-				</a>
-				<a href="/technical-details/" aria-label="Info for IT">
-					<div class="menu-overlay-link">Info for IT teams</div>
-				</a>
-				<a href="/data-storage-integrations-security/" aria-label="Data integrations, storage and security">
-					<div class="menu-overlay-link">Data &  integrations</div>
-				</a>
-				<a href="/security/" aria-label="Security">
-					<div class="menu-overlay-link">Security</div>
-				</a>
+			<div class="menu-overlay-title">MAIN MENU</div>
+				<?php
+					wp_nav_menu(
+						array(
+							'theme_location' => 'main_menu_mobile',
+							'menu_class' => 'main-mobile-menu',
+							'container' => 'div',
+							'container_class' => 'menu-overlay-list',
+							'walker' => new Main_Mobile_Menu_Walker(),
+						)
+					);
+				?>
 			</div>
-			<div class="menu-overlay-title">RESOURCES</div>
 			<div class="menu-overlay-footer">
-				<div class="menu-overlay-column-1">
-					<a href="https://help.fliplet.com/" target="_blank" aria-label="Fliplet Support Site"><div class="menu-overlay-footerlink">Support site</div></a>
-					<a href="http://developers.fliplet.com" target="_blank" aria-label="Fliplet Developer Site"><div class="menu-overlay-footerlink">Developer site</div></a>
-					<a href="/fliplet-viewer" aria-label="Download Fliplet Viewer"><div class="menu-overlay-footerlink">Fliplet viewer</div></a>
-					<a href="/contact" aria-label="Contact us"><div class="menu-overlay-footerlink">Contact us</div></a>
-				</div>
-				<div class="menu-overlay-column">
-					<a href="/fliplet-careers" aria-label="Careers"><div class="menu-overlay-footerlink">Careers</div></a>
-					<a href="/webinars" aria-label="Webinars"><div class="menu-overlay-footerlink">Webinars</div></a>
-					<a href="/reports" aria-label="Reports"><div class="menu-overlay-footerlink">Reports</div></a>
-					<a href="/blog" aria-label="Blog"><div class="menu-overlay-footerlink">Blog</div></a>
-				</div>
+				<div class="menu-overlay-title">RESOURCES</div>
+				<?php
+					wp_nav_menu(
+						array(
+							'theme_location' => 'resources_menu_mobile',
+							'menu_class' => 'resources-mobile-menu',
+							'container' => 'div',
+							'container_class' => 'menu-footer-holder',
+							'walker' => new Main_Mobile_Menu_Walker(),
+						)
+					);
+				?>
 			</div>
 		</div>
 	</header>
--- inc/view/template-menus.php
+++ inc/view/template-menus.php
@@ -4,9 +4,14 @@
 if ( function_exists( 'register_nav_menus' ) ) {
 	register_nav_menus(
 		array(
-		  'main_menu'               => 'Main Menu',
-		  'blog_menu'             => 'Blog Menu',
-		  'contact_info' 				=> 'Contact information'
+			'main_menu' => 'Main Menu',
+			'main_menu_mobile' => 'Main Menu Mobile',
+			'resources_menu_mobile' => 'Resources Menu Mobile',
+		  'blog_menu' => 'Blog Menu',
+			'contact_info' => 'Contact information',
+			'footer_menu_one' => 'Footer Menu One',
+			'footer_menu_two' => 'Footer Menu Two',
+			'footer_menu_three' => 'Footer Menu Three'
 		)
 	);
 }
